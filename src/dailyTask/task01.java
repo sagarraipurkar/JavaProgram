3ad23a1e671f2ddc235dfa54ab12638e7fe69eb8
sagarraipurkar <er.sagarraipurkar@gmail.com>
2024-10-04 06:45:39 +0530

    Changes in the task01 class

--- src/dailyTask/task01.java
+++ src/dailyTask/task01.java
@@ -10,6 +10,9 @@ public class task01 {
         System.out.println("10x4 = 40");
         System.out.println("10x5 = 50");
         System.out.println("10x6 = 60");
-
+        System.out.println("10x7 = 70");
+        System.out.println("10x8 = 80");
+        System.out.println("10x9 = 90");
+        System.out.println("10x10 = 100");
     }
 }
